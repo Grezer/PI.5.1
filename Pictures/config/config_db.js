3a510fbe6aa93460984a88f813ba701369615992
Name <rzaevnr@list.ru>
2019-10-22 14:02:30 +0300

    Start

---
+++ Pictures/config/config_db.js
@@ -0,0 +1,10 @@
+const sql = require('mysql2/promise');
+
+const pool = sql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS
+});
+
+module.exports = pool;
