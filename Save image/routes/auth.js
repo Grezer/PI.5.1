e61ab550f167bc958281a796e730db5d9fc71340
Name <rzaevnr@list.ru>
2019-11-17 14:46:40 +0300

    Register and login ready

--- Save image/routes/auth.js
+++ Save image/routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const pool = require('../config/config_db');
 const Joi = require('@hapi/joi');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // const schema = {
 //   nickName: Joi.string()
@@ -50,6 +51,25 @@ router.post('/register', async (req, res) => {
       console.log('123123123' + err);
       res.sendStatus(400).send(err);
     });
+});
+
+router.post('/login', async (req, res) => {
+  const promisePool = pool.promise();
+  const findLogin = 'Select * FROM people Where login = ?';
+  const [loginExist] = await promisePool.query(findLogin, req.body.login);
+
+  if (loginExist.length == 0)
+    return res.status(400).send('Login or password is wrong');
+
+  const validPass = await bcrypt.compare(
+    req.body.password,
+    loginExist[0].passwordHash
+  );
+
+  if (validPass) return res.status(400).send('Login or password is wrong');
+
+  const token = jwt.sign({ id: loginExist[0].id, role: loginExist[0].role}, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
--- Save image/routes/testApi.js
+++ Save image/routes/testApi.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const pool = require('../config/config_db')
+const verify = require('./verifyToken')
 
 
-router.route('/test').get((req, res, next) => {
+
+
+
+router.route('/test').get(verify, (req, res, next) => {
+
   pool.query("SELECT * FROM people", function (err, results) {
     if (err) console.log(err);
     res.send(results);
